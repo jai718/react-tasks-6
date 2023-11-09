5f5c7bcfda725b4c008b3f6b7e836d5ba94a936b
jai718 <143997498+jai718@users.noreply.github.com>
2023-11-09 20:36:51 +0530

    Add files via upload

---
+++ Nav.js
@@ -0,0 +1,42 @@
+function Nav() {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+      <h1>
+        <a
+          class="navbar-brand text-primary mx-4"
+          href="KOPPISETTI VENKATA KRISHNA NAIDU"
+        >
+          KOPPISETTI VENKATA KRISHNA NAIDU
+        </a>
+      </h1>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto">
+          <a class="nav-item nav-link px-4 " href="#maindiv">
+            Home
+          </a>
+          <a class="nav-item nav-link px-4 " href="#AboutMe">
+            About
+          </a>
+          <a class="nav-item nav-link px-4 " href="#Projects">
+            Projects
+          </a>
+          <a href="https://drive.google.com/file/d/1LOzv0VfMBi-larEkk8vjOUDIoL4JcKGW/view?usp=sharing">
+            <button class="btn btn-primary mx-4">Resume</button>
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+export default Nav;
